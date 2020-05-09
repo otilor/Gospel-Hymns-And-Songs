62373dcc8512f50739b892a55dfc0c2b4df4436e
GabielFemi <gabrielfemi799@gmail.com>
2020-05-09 23:10:41 +0200

    remove dedundant line

--- ghs.go
+++ ghs.go
@@ -4,19 +4,7 @@ import (
 	"fmt"
 	"net/http"
 )
-type GHS struct {
-	id int
-	title string
-	chorus string
-	stanza_1 string
-	stanza_2 string
-	stanza_3 string
-	stanza_4 string
-	stanza_5 string
-	stanza_6 string
-	stanza_7 string
-	stanza_8 string
-}
+
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	db := dbConn()
@@ -25,7 +13,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		panic(err.Error())
 	}
 	ghs := GHS{}
-
+	ghsSlice := []GHS{}
 	for fetchAllSongs.Next() {
 		var id int
 		var title, chorus, stanza_1, stanza_2, stanza_3, stanza_4, stanza_5, stanza_6, stanza_7, stanza_8 string
@@ -34,21 +22,59 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err.Error())
 		}
-		ghs.id = id
-		ghs.title = title
-		ghs.chorus = chorus
-		ghs.stanza_1= stanza_1
-		ghs.stanza_2 = stanza_2
-		ghs.stanza_3 = stanza_3
-		ghs.stanza_4= stanza_4
-		ghs.stanza_5 = stanza_5
-		ghs.stanza_6 = stanza_6
-		ghs.stanza_7= stanza_7
-		ghs.stanza_8 = stanza_8
+		ghs.Id = id
+		ghs.Title = title
+		ghs.Chorus = chorus
+		ghs.Stanza_1= stanza_1
+		ghs.Stanza_2 = stanza_2
+		ghs.Stanza_3 = stanza_3
+		ghs.Stanza_4= stanza_4
+		ghs.Stanza_5 = stanza_5
+		ghs.Stanza_6 = stanza_6
+		ghs.Stanza_7= stanza_7
+		ghs.Stanza_8 = stanza_8
+		
+		ghsSlice = append(ghsSlice, ghs)
 
 	}
-	fmt.Println(ghs)
-	render(w, "index.html", nil)
+
+	render(w, "index.html", ghsSlice)
 	
 }
 
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	Id := r.URL.Query().Get("id")
+	findGhs, err := db.Query("SELECT * FROM songs WHERE id = ?", Id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ghs := GHS{}
+	ghsSlice := []GHS{}
+
+	for findGhs.Next() {
+		var id int
+		var title, chorus, stanza_1, stanza_2, stanza_3, stanza_4, stanza_5, stanza_6, stanza_7, stanza_8 string
+		err = findGhs.Scan(&id, &title, &chorus, &stanza_1, &stanza_2, &stanza_3, &stanza_4, &stanza_5, &stanza_6, &stanza_7, &stanza_8)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		ghs.Id = id
+		ghs.Title = title
+		ghs.Chorus = chorus
+		ghs.Stanza_1= stanza_1
+		ghs.Stanza_2 = stanza_2
+		ghs.Stanza_3 = stanza_3
+		ghs.Stanza_4= stanza_4
+		ghs.Stanza_5 = stanza_5
+		ghs.Stanza_6 = stanza_6
+		ghs.Stanza_7= stanza_7
+		ghs.Stanza_8 = stanza_8
+		
+		ghsSlice = append(ghsSlice, ghs)
+		
+	}
+	render(w, "show.html", ghsSlice)
+}
--- view.go
+++ view.go
@@ -8,14 +8,29 @@ import (
 	"net/http"
 )
 
-func render(w http.ResponseWriter, tmpl string, r *http.Request){
+type GHS struct {
+	Id int
+	Title string
+	Chorus string
+	Stanza_1 string
+	Stanza_2 string
+	Stanza_3 string
+	Stanza_4 string
+	Stanza_5 string
+	Stanza_6 string
+	Stanza_7 string
+	Stanza_8 string
+}
+
+
+func render(w http.ResponseWriter, tmpl string, pageData []GHS){
 	tmpl = fmt.Sprintf("form/%s", tmpl)
 	t, err := template.ParseFiles(tmpl)
 	if err != nil {
 		log.Print("template parsing error: ", err.Error())
 	}
 
-	err = t.Execute(w, nil)
+	err = t.Execute(w, pageData)
 		if err != nil {
 			log.Print("template exeuting error", err)
 		}
